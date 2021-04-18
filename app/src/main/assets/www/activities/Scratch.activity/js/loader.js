790185013f70d5bd9a11f8d4f6fdb2f5e9d5b9b0
Lionel Laské <llaske@c2s.fr>
2021-04-18 16:18:28 +0200

    Remove embedded ressources

--- app/src/main/assets/www/activities/Scratch.activity/js/loader.js
+++ app/src/main/assets/www/activities/Scratch.activity/js/loader.js
@@ -1,1 +1,1 @@
-requirejs.config({baseUrl:"lib",paths:{activity:"../js"}}),requirejs(["activity/activity"]);
+window._isOffline=document.getElementById("myBlocks")&&"offlinemode"==document.getElementById("myBlocks").className,requirejs.config({baseUrl:"lib",paths:{activity:"../js"}}),requirejs(["activity/activity"]);
